d9b50c31fe8b42d6b6c8ef30592588205960b2f9
elpastorr <89456448+elpastorr@users.noreply.github.com>
2023-01-30 16:52:41 +0100

    Add files via upload

--- microshell.c
+++ microshell.c
@@ -1,225 +1,95 @@
-#include <stdlib.h>
 #include <unistd.h>
-#include <string.h>  
+#include <string.h>
+#include <sys/wait.h>
 
-#define TYPE_END	0
-#define TYPE_PIPE	1
-#define TYPE_BREAK	2
-
-typedef struct s_list
+void	ft_putstr_err(char *str)
 {
-    char    **args;
-    int     length;
-    int     type;
-    int     pipe[2];
-    struct s_list   *previous;
-    struct s_list   *next;
-}   t_list;
-
-int ft_strlen(char *s)
-{
-    int i = 0;
-
-    if (!s)
-        return (0);
-    while (s[i])
-        i++;
-    return (i);
-}
-
-void    put_error(char *s)
-{
-    write(2, s, ft_strlen(s));
-}
-
-void    exit_fatal(void)
-{
-    put_error("error: fatal\n");
-    exit(1);
-}
-
-char	*ft_strdup(char *s)
-{
-	char	*copy;
 	int	i;
 
-	if(!(copy = (char *)malloc(ft_strlen(s) + 1)))
-        exit_fatal();
-	i = -1;
-	while (s[++i])
-		copy[i] = s[i];
-	copy[i] = 0;
-	return (copy);
+	i = 0;
+	while (str[i])
+		i++;
+	write(2, str, i);
 }
 
-int add_arg(t_list *cmd, char *arg)
+int	ft_execute(char **argv, int i, int tmp_fd, char **env)
 {
-    char    **tmp = NULL;
-    int     i = 0;
-
-    if (!(tmp = (char **)malloc(sizeof(*tmp) * (cmd->length + 2))))
-        exit_fatal();
-    while (i < cmd->length)
-    {
-        tmp[i] = cmd->args[i];
-        i++;
-    }
-    if (cmd->length > 0)
-        free(cmd->args);
-    cmd->args = tmp;
-    cmd->args[i++] = ft_strdup(arg);
-    cmd->args[i] = 0;
-    cmd->length++;
-    return (0);
+	argv[i] = NULL;
+	close(tmp_fd);
+	execve(argv[0], argv, env);
+	ft_putstr_err("error: cannot execute ");
+	ft_putstr_err(argv[0]);
+	write(2, "\n", 1);
+	return (1);
 }
 
-int list_push(t_list **cmds, char *arg)
+int	main(int argc, char **argv, char **env)
 {
-    t_list  *new;
+	int	i;
+	int	tmp_fd;
+	int	fd[2];
+	int	pid;
 
-    if (!(new = (t_list *)malloc(sizeof(*new))))
-        exit_fatal();
-    new->args = NULL;
-    new->length = 0;
-    new->type = TYPE_END;
-    new->previous = NULL;
-    new->next = NULL;
-    if (*cmds)
-    {
-        (*cmds)->next = new;
-        new->previous = *cmds;
-    }
-    *cmds = new;
-    return(add_arg(new, arg));
-}
-
-void list_clear(t_list **cmds)
-{
-    t_list  *tmp;
-    int     i;
-
-    while (*cmds && (*cmds)->previous)
-        *cmds = (*cmds)->previous;
-    while (*cmds)
-    {
-        tmp = (*cmds)->next;
-        i = 0;
-        while (i < (*cmds)->length)
-            free((*cmds)->args[i++]);
-        free((*cmds)->args);
-        free(*cmds);
-        *cmds = tmp;
-    }
-    *cmds = NULL;
-}
-
-int    parse(t_list **cmds, char *arg)
-{
-    int isbreak = (strcmp(";", arg) == 0);
-
-    if (isbreak && !*cmds)
-        return (0);
-    if (!isbreak && (!*cmds || (*cmds)->type > TYPE_END))
-        return (list_push(cmds, arg));
-    if (strcmp("|", arg) == 0)
-        (*cmds)->type = TYPE_PIPE;
-    else if (isbreak)
-        (*cmds)->type = TYPE_BREAK;
-    else
-        return (add_arg(*cmds, arg));
-    return (0);
-}
-
-int exec_cmd(t_list *cmd, char **env)
-{
-    pid_t   pid;
-    int     exits = 1;
-    int     status;
-    int     pipe_open = 0;
-
-    if (cmd->type == TYPE_PIPE || (cmd->previous && cmd->previous->type == TYPE_PIPE))
-    {
-        pipe_open = 1;
-        if (pipe(cmd->pipe))
-            exit_fatal();
-    }
-    pid = fork();
-    if (pid < 0)
-        exit_fatal();
-    if (pid == 0)
-    {
-        if (cmd->type == TYPE_PIPE && dup2(cmd->pipe[1], 1) < 0)
-            exit_fatal();
-        if (cmd->previous && cmd->previous->type == TYPE_PIPE && dup2(cmd->previous->pipe[0], 0) < 0)
-            exit_fatal();
-        if ((exits = execve(cmd->args[0], cmd->args, env)) < 0)
-        {
-            put_error("error: cannot execute ");
-            put_error(cmd->args[0]);
-            put_error("\n");
-        }
-        exit(exits);
-    }
-    else
-    {
-        waitpid(pid, &status, 0);
-        if (pipe_open)
-        {
-            close(cmd->pipe[1]);
-            if (!cmd->next || cmd->type == TYPE_BREAK)
-                close(cmd->pipe[0]);
-        }
-        if (cmd->previous && cmd->previous->type == TYPE_PIPE)
-            close(cmd->previous->pipe[0]);
-        if (WIFEXITED(status))
-            exits = WEXITSTATUS(status);
-    }
-    return (exits);
-}
-
-int exec_cmds(t_list **cmds, char **env)
-{
-    int exit = 0;
-
-    while (*cmds && (*cmds)->previous)
-        *cmds = (*cmds)->previous;
-    while (*cmds)
-    {
-        if (strcmp("cd", (*cmds)->args[0]) == 0)
-        {
-            exit = 0;
-            if ((*cmds)->length < 2)
-            {
-                put_error("error: cd: bad arguments\n");
-                exit = 1;
-            }
-            else if (chdir((*cmds)->args[1]))
-            {
-                exit = 1;
-                put_error("error: cd: cannot change directory to ");
-                put_error((*cmds)->args[1]);
-                put_error("\n");
-            }
-        }
-        else
-            exit = exec_cmd(*cmds, env);
-        *cmds = (*cmds)->next;
-    }
-    return (exit);
-}
-
-int main(int ac, char **av, char **env)
-{
-    t_list  *cmds;
-    int i;
-    int ret;
-
-    cmds = NULL;
-    i = 1;
-    while (i < ac)
-        parse(&cmds, av[i++]);
-    if (cmds)
-        ret = exec_cmds(&cmds, env);
-    list_clear(&cmds);
-    return (ret);
+	(void)argc;
+	i = 0;
+	pid = 0;
+	tmp_fd = dup(STDIN_FILENO);
+	while (argv[i] && argv[i + 1])
+	{
+		argv = &argv[i + 1];
+		i = 0;
+		while (argv[i] && strcmp(argv[i], ";") && strcmp(argv[i], "|"))
+			i++;
+		if (strcmp(argv[0], "cd") == 0)
+		{
+			if (i != 2)
+				ft_putstr_err("error: cd: bad arguments\n");
+			else if (chdir(argv[1]) != 0)
+			{
+				ft_putstr_err("error: cd: cannot execute ");
+				ft_putstr_err(argv[1]);
+				write(2, "\n", 1);
+			}
+		}
+		else if (argv != &argv[i] && (argv[i] == NULL || !strcmp(argv[i], ";")))
+		{
+			pid = fork();
+			if (pid == 0)
+			{
+				dup2(tmp_fd, STDIN_FILENO);
+				if (ft_execute(argv, i, tmp_fd, env))
+					return (1);
+			}
+			else
+			{
+				close(tmp_fd);
+				waitpid(-1, NULL, WUNTRACED);
+				tmp_fd = dup(STDIN_FILENO);
+			}
+		}
+		else if (argv != &argv[i] && !strcmp(argv[i], "|"))
+		{
+			pipe (fd);
+			pid = fork();
+			if (pid == 0)
+			{
+				dup2(tmp_fd, STDIN_FILENO);
+				dup2(fd[1], STDOUT_FILENO);
+				close(fd[1]);
+				close(tmp_fd);
+				if (ft_execute(argv, i, tmp_fd, env))
+					return (1);
+			}
+			else
+			{
+				close(tmp_fd);
+				close(fd[1]);
+				waitpid(-1, NULL, WUNTRACED);
+				tmp_fd = dup(fd[0]);
+				close(fd[0]);
+			}
+		}
+	}
+	close(tmp_fd);
+	return (0);
 }
